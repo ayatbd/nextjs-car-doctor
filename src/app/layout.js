3c21717f4e09977f1ce11123ee29e5e604f10087
A Behesti <albehestione@gmail.com>
2025-09-13 17:04:33 +0600

    added navbar and some services card

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`max-w-[1140px] mx-auto ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar></Navbar>
         {children}
